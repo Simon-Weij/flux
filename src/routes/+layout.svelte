2db238198fe5695beb87c8bc4bb431f45a08e5fa
Simon <175155691+Simon-Weij@users.noreply.github.com>
2026-01-02 14:43:26 +0100

    sorted css classes and improved titlebar logic

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,12 +14,11 @@
   import { onMount, onDestroy } from "svelte";
   import { getCurrentWindow } from "@tauri-apps/api/window";
   import MinusIcon from "../icons/MinusIcon.svelte";
+  import UserIcon from "../icons/UserIcon.svelte";
 
   let appWindow: ReturnType<typeof getCurrentWindow>;
   let isMaximized = false;
   let unlisten: (() => void) | null = null;
-  let isDragging = false;
-  let dragTimeout: number;
 
   onMount(async () => {
     appWindow = getCurrentWindow();
@@ -35,7 +34,6 @@
     if (unlisten) {
       unlisten();
     }
-    clearTimeout(dragTimeout);
   });
 
   function minimize() {
@@ -49,53 +47,48 @@
   function close() {
     appWindow?.close();
   }
-
-  async function handleTitlebarDoubleClick(event: MouseEvent) {
-    if (!isDragging) {
-      event.preventDefault();
-      event.stopPropagation();
-      await toggleMaximize();
-    }
-  }
-
-  function handleMouseDown() {
-    isDragging = false;
-    dragTimeout = window.setTimeout(() => {
-      isDragging = true;
-    }, 150);
-  }
-
-  function handleMouseUp() {
-    clearTimeout(dragTimeout);
-  }
 </script>
 
 <div class="h-screen flex flex-col bg-[#151515] text-white">
   <!-- Titlebar -->
   <div
-    class="h-12 select-none grid grid-cols-[auto_max-content] fixed top-0 left-0 right-0 z-50 bg-[#2e2e2e] shadow-md"
+    class="h-12 select-none grid grid-cols-[max-content_auto_max-content] fixed top-0 left-0 right-0 z-50 bg-[#2e2e2e] shadow-md"
   >
+    <div class="flex">
+      <button
+        title="user"
+        class="inline-flex items-center justify-center w-8 p-0 m-0 transition-colors bg-transparent border-none appearance-none cursor-pointer hover:bg-blue-600"
+      >
+        <UserIcon />
+      </button>
+    </div>
     <div
-      data-tauri-drag-region
+      id="titlebar"
       class="flex-1"
-      ondblclick={handleTitlebarDoubleClick}
-      onmousedown={handleMouseDown}
-      onmouseup={handleMouseUp}
       role="button"
       tabindex="-1"
+      onmousedown={(e) => {
+        if (e.buttons === 1) {
+          if (e.detail === 2) {
+            toggleMaximize();
+          } else {
+            appWindow?.startDragging();
+          }
+        }
+      }}
     ></div>
     <div class="flex">
       <button
         onclick={minimize}
         title="minimize"
-        class="appearance-none p-0 m-0 border-none inline-flex justify-center items-center w-8 bg-transparent hover:bg-blue-600 transition-colors"
+        class="inline-flex items-center justify-center w-8 p-0 m-0 transition-colors bg-transparent border-none appearance-none cursor-pointer hover:bg-blue-600"
       >
         <MinusIcon />
       </button>
       <button
         onclick={toggleMaximize}
         title="maximize"
-        class="appearance-none p-0 m-0 border-none inline-flex justify-center items-center w-8 bg-transparent hover:bg-blue-600 transition-colors"
+        class="inline-flex items-center justify-center w-8 p-0 m-0 transition-colors bg-transparent border-none appearance-none cursor-pointer hover:bg-blue-600"
       >
         {#if isMaximized}
           <RestoreIcon />
@@ -106,14 +99,14 @@
       <button
         onclick={close}
         title="close"
-        class="appearance-none p-0 m-0 border-none inline-flex justify-center items-center w-8 bg-transparent hover:bg-red-600 transition-colors"
+        class="inline-flex items-center justify-center w-8 p-0 m-0 transition-colors bg-transparent border-none appearance-none cursor-pointer hover:bg-red-600"
       >
         <CloseIcon />
       </button>
     </div>
   </div>
 
-  <div class="pt-12 flex-1 overflow-auto">
+  <div class="flex-1 pt-12 overflow-auto">
     <slot />
   </div>
 </div>
